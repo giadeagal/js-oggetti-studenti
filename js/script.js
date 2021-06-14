3cb1592ab2436535bccc252f7663014a78b0f059
Gianluca <gl.galassini@gmail.com>
2021-06-14 18:35:10 +0200

    aggiunta studente

--- js/script.js
+++ js/script.js
@@ -5,39 +5,35 @@
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 docuPrint("title", "<h1>Elenco Studenti</h1>");
 
-// var studentA = {
-//     name: "Bilbo",
-//     surname: "Baggins",
-//     age: 111
-// };
-
 var students = [
     {
-        name : "Bilbo",
-        surname : "Baggins"
+        name: "Bilbo",
+        surname: "Baggins"
     },
     {
-        name : "Frodo",
-        surname : "Baggins"
+        name: "Frodo",
+        surname: "Baggins"
     },
     {
-        name : "Gandalf",
-        surname : "The White"
+        name: "Gandalf",
+        surname: "The White"
     },
     {
-        name : "Radagast",
-        surname : "The Brown"
+        name: "Radagast",
+        surname: "The Brown"
     },
     {
-        name : "Thorin",
-        surname : "Oakshield"
+        name: "Thorin",
+        surname: "Oakshield"
     },
 ];
 
 for (var i = 0; i < students.length; i++) {
-    var listItem = "<li>" + students[i].name + " " + students[i].surname+ "</li><br>";
+    var listItem = "<li>" + "Nome: " + students[i].name + "<br>Cognome: " + students[i].surname+ "</li><br>";
     docuAdd("list", listItem);
 };
+
+document.getElementById("addBtn").addEventListener("click", addStudent);
 
 // FUNZIONI **********
 function docuPrint(x, y) {
@@ -46,4 +42,21 @@ document.getElementById(x).innerHTML = y;
 
 function docuAdd(x, y) {
     document.getElementById(x).innerHTML += y;
+}
+
+function addStudent() {
+    students.push(
+        {
+            name: prompt("Inserisci il nome del nuovo studente"),
+            surname: prompt("Inserisci il cognome del nuovo studente"),
+        }
+    )
+    document.getElementById("list").className = "d-none";
+    document.getElementById("addBtn").className = "d-none";
+    document.getElementById("newList").className -= "d-none";
+
+    for (var i = 0; i < students.length; i++) {
+        var listItem = "<li>" + "Nome: " + students[i].name + "<br>Cognome: " + students[i].surname+ "</li><br>";
+        docuAdd("newList", listItem);
+    };
 }
